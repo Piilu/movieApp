fbb4d26acc6c33add17457a78cbf357c565c99f3
Rainer Piil <piilrainer@gmail.com>
2022-10-01 12:06:57 +0300

    Added movies

---
+++ schoolWebApp/Data/schoolWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using schoolWebApp.Models;
+
+namespace schoolWebApp.Data
+{
+    public class schoolWebAppContext : DbContext
+    {
+        public schoolWebAppContext (DbContextOptions<schoolWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<schoolWebApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
